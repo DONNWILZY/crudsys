5a68f055d16f3f7a26a642d49ef137e5315d129b
donnwilzy <ask4godswill@gmail.com>
2023-01-03 13:04:28 +0100

    added route and views

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 
 const express = require('express');
-const port = 3000;
+const port = process.env.port || 3000;
 const ejs = require('ejs');
 const bodyParser = require('body-Parser');
 app = express();
 const mongoose = require('mongoose');
 //const bodyParser = require('body-parser');
 
+
+//data base connection
 mongoose.connect('mongodb://127.0.0.1:27017', (err)=>{
     if(!err)console.log('db connected');
     else console.log('CAN NOT CONNECT TO DB');
 })
 
+app.set('view engine', 'ejs');
 
-app.use('/', (req, res)=>{
-    res.send('we are live')
-})
+const homepageRoutes = require('./routers/homepage')
 
+//roroute and views for testing
+app.use('/', homepageRoutes)
+
+
+// server/
 app.listen(port, ()=>console.log(`Port ${port} activated: || http://127.0.0.1:${port}/`))
